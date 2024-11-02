c8bc0b1966d456cae43a4cc60072e5c147d3d821
Tommy Nordum <tommy@spindelvev.net>
2024-11-02 15:17:10 +0100

    Basic setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
+
+import "/styles/index.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
