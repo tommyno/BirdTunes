83922fd96fdd41e67ff10e3286d420d82d7efaa6
Tommy Nordum <tommy@spindelvev.net>
2025-01-06 21:22:38 +0100

    Tweak settings

--- components/Settings/Settings.tsx
+++ components/Settings/Settings.tsx
@@ -1,8 +1,6 @@
-import React, { useState, useEffect } from "react";
-import { useRouter } from "next/router";
+import React from "react";
 import styles from "./Settings.module.scss";
 import { Species } from "hooks/useFetchSpecies";
-import { Input } from "components/Input/Input";
 import Link from "next/link";
 
 type Props = {
@@ -25,21 +23,24 @@ export const Settings: React.FC<Props> = ({
 
   return (
     <div className={styles.wrap}>
+      {stationId && (
+        <p>
+          <strong>
+            <Link
+              className="link"
+              href={`https://app.birdweather.com/stations/${stationId}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {stationName}
+            </Link>
+          </strong>
+        </p>
+      )}
+
       <p>
         {totalDetections} observasjoner · {speciesData?.length || 0} arter
       </p>
-      {stationId && (
-        <p>
-          <Link
-            className="link"
-            href={`https://app.birdweather.com/stations/${stationId}`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {stationName}
-          </Link>
-        </p>
-      )}
 
       {speciesError && <p>{speciesError.toString()}</p>}
     </div>
