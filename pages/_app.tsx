31899684725b42b5ff64bd4436bde53081884972
Tommy Nordum <tommy@spindelvev.net>
2025-01-12 21:25:13 +0100

    Add stats

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { ModalProvider } from "contexts/ModalContext";
+import Script from "next/script";
 
 import "/styles/index.scss";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
@@ -11,9 +13,16 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta charSet="utf-8" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <ModalProvider>
         <Component {...pageProps} />
       </ModalProvider>
+
+      <Script
+        src="https://cloud.umami.is/script.js"
+        data-website-id="fb88d8e1-5cd5-4760-b4ea-378c12c6301c"
+        strategy="lazyOnload"
+      />
     </>
   );
 }
