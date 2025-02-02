4b0dc13fca7447a297a9670f9850a913f3bc5500
Tommy Nordum <tommy@spindelvev.net>
2025-02-02 20:54:48 +0100

    Add favicon for all devices

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,7 +14,21 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
-        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <meta name="apple-mobile-web-app-title" content="BirdTunes" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       <ModalProvider>
