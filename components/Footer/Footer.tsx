ec734a2d0fada8d75a965a2084a8bd7bd7ce8ca9
tommyno <tommy@spindelvev.net>
2026-01-07 10:56:23 +0100

    Link header and footer logo to front page

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,11 +1,17 @@
 import React from "react";
+import Link from "next/link";
 
 import styles from "./Footer.module.scss";
+import { useQueryParam } from "hooks/useQueryParams";
 
 export const Footer: React.FC = () => {
+  const [lang] = useQueryParam({ key: "lang" });
+
   return (
     <div className={styles.footer}>
-      <p>BirdTunes</p>
+      <p>
+        <Link href={`/${lang ? `?lang=${lang}` : ""}`}>BirdTunes</Link>
+      </p>
       <a
         href="https://github.com/tommyno/birdtunes"
         className="link"
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,11 +2,17 @@ import React from "react";
 
 import styles from "./Header.module.scss";
 import { LanguageSwitcher } from "components/LanguageSwitcher";
+import Link from "next/link";
+import { useQueryParam } from "hooks/useQueryParams";
 
 export const Header: React.FC = () => {
+  const [lang] = useQueryParam({ key: "lang" });
+
   return (
     <div className={styles.header}>
-      <h1 className="h3">BirdTunes</h1>
+      <h1 className="h3">
+        <Link href={`/${lang ? `?lang=${lang}` : ""}`}>BirdTunes</Link>
+      </h1>
       <LanguageSwitcher />
     </div>
   );
